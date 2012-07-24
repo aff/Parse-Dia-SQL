8bcca8967c384542abaa79506f8e4dfc3d52663a
Andreas Faafeng <aff@cpan.org>
2012-07-24 09:41:09 +0200

    Added new version number 0.21 to Changes

--- lib/Parse/Dia/SQL.pm
+++ lib/Parse/Dia/SQL.pm
@@ -200,7 +200,7 @@ use Parse::Dia::SQL::Output::SQLite3fk;
 use Parse::Dia::SQL::Output::Sas;
 use Parse::Dia::SQL::Output::Sybase;
 
-our $VERSION = '0.20_02';
+our $VERSION = '0.21';
 
 my $UML_ASSOCIATION  = 'UML - Association';
 my $UML_SMALLPACKAGE = 'UML - SmallPackage';
