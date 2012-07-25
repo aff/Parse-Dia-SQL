7555a73e93d9de1749a0d16989dbb45f102004f1
Andreas Faafeng <aff@cpan.org>
2012-07-25 13:27:14 +0200

    Version 0.21_01: - Let 'make dist' generated META.yml automatically.

--- lib/Parse/Dia/SQL.pm
+++ lib/Parse/Dia/SQL.pm
@@ -200,7 +200,7 @@ use Parse::Dia::SQL::Output::SQLite3fk;
 use Parse::Dia::SQL::Output::Sas;
 use Parse::Dia::SQL::Output::Sybase;
 
-our $VERSION = '0.21';
+our $VERSION = '0.21_01';
 
 my $UML_ASSOCIATION  = 'UML - Association';
 my $UML_SMALLPACKAGE = 'UML - SmallPackage';
