de88599d1035458af05191d9b1ec4798f04a8589
aff <aff@cpan.org>
2020-06-17 22:33:44 +0200

    Version 0.31, merged CR #9.

--- lib/Parse/Dia/SQL.pm
+++ lib/Parse/Dia/SQL.pm
@@ -200,7 +200,7 @@ use Parse::Dia::SQL::Output::SQLite3fk;
 use Parse::Dia::SQL::Output::Sas;
 use Parse::Dia::SQL::Output::Sybase;
 
-our $VERSION = '0.30';
+our $VERSION = '0.31';
 
 my $UML_ASSOCIATION  = 'UML - Association';
 my $UML_SMALLPACKAGE = 'UML - SmallPackage';
